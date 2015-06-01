1eb0aa220a668d5ce5b7fb120beefc6529767681
Connor Locke-Warburton <clocke555@gmail.com>
2015-06-01 19:47:29 +1000

    ~ Fixed Mana ui issue with differing res

--- Assets/Scripts/ManaUI.cs
+++ Assets/Scripts/ManaUI.cs
@@ -5,51 +5,50 @@ using System.Collections;
 public class ManaUI : MonoBehaviour {
 
 	public PlayerContoller AttachedPlayer;
+	public Image BackgroundBarToScaleTo;
 	public Image ManaBar;
 	public Image CurrentSpellBar;
 	public float LerpFactor = 0.2f;
-	
-	private float LeftManaPosition;
-	private float RightManaPosition;
+
 	private float MaxManaScale;
 	
 	// Use this for initialization
 	void Start () {
-		LeftManaPosition = ManaBar.rectTransform.position.x;
-		MaxManaScale = ManaBar.rectTransform.sizeDelta.x;
-		RightManaPosition = LeftManaPosition + MaxManaScale;
+		MaxManaScale = BackgroundBarToScaleTo.rectTransform.rect.xMax;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		MaxManaScale = BackgroundBarToScaleTo.rectTransform.rect.width;
+
 		float CurrMinManaPower = AttachedPlayer.CurrMinManaPower;
 		float CurrMaxManaPower = AttachedPlayer.CurrMaxManaPower;
 		float MaxManaPower = AttachedPlayer.MaxManaPower;
 		
 		float CurPower = AttachedPlayer.SpellHoldTime;
 		
-		float ManaLeftX = (CurrMinManaPower / MaxManaPower) * MaxManaScale + LeftManaPosition;
-		float ManaRightX = (CurrMaxManaPower / MaxManaPower) * MaxManaScale + LeftManaPosition;
-		float ManaWidth = ManaRightX - ManaLeftX;
+		float ManaLeftX = (CurrMinManaPower / MaxManaPower) * MaxManaScale;
+		float ManaRightX = (CurrMaxManaPower / MaxManaPower) * MaxManaScale;
 		
-		float CurPowerRightX = (CurPower / MaxManaPower) * MaxManaScale + LeftManaPosition;
-		float PowerWidth = CurPowerRightX - ManaLeftX;
+		float CurPowerRightX = (CurPower / MaxManaPower) * MaxManaScale;
 
 
-		ManaBar.rectTransform.position = Vector3.Lerp(	ManaBar.rectTransform.position,
-													  	new Vector3(ManaLeftX, ManaBar.rectTransform.position.y, ManaBar.rectTransform.position.z),
+		ManaBar.rectTransform.offsetMin = Vector2.Lerp(	ManaBar.rectTransform.offsetMin,
+		                                                new Vector2(ManaLeftX, ManaBar.rectTransform.offsetMin.y),
 		                                              	LerpFactor);
 													  
-		ManaBar.rectTransform.sizeDelta = Vector2.Lerp(	ManaBar.rectTransform.sizeDelta,
-														new Vector2(ManaWidth, ManaBar.rectTransform.sizeDelta.y),
+		ManaBar.rectTransform.offsetMax = Vector2.Lerp(	ManaBar.rectTransform.offsetMax,
+		                                               new Vector2(ManaRightX, ManaBar.rectTransform.offsetMax.y),
 		                                               	LerpFactor);
+		                                               	
+		CurrentSpellBar.rectTransform.offsetMin = Vector2.Lerp(	CurrentSpellBar.rectTransform.offsetMin,
+		                                                       new Vector2(ManaLeftX, CurrentSpellBar.rectTransform.offsetMin.y),
+		                                               			LerpFactor);
 		
-		CurrentSpellBar.rectTransform.position = Vector3.Lerp(	CurrentSpellBar.rectTransform.position,
-																new Vector3(ManaLeftX, ManaBar.rectTransform.position.y, ManaBar.rectTransform.position.z),
-		                                                      	LerpFactor);
-																
-		CurrentSpellBar.rectTransform.sizeDelta = new Vector2(PowerWidth, CurrentSpellBar.rectTransform.sizeDelta.y);		
+		CurrentSpellBar.rectTransform.offsetMax = Vector2.Lerp(	CurrentSpellBar.rectTransform.offsetMax,
+		                                                       new Vector2(CurPowerRightX, CurrentSpellBar.rectTransform.offsetMax.y),
+		                                               			LerpFactor);
 		
 	}
 }
