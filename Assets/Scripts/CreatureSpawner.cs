11ec856fdf0d4bee2a6c84958917c3ee98ed71b3
Connor Locke-Warburton <clocke555@gmail.com>
2015-06-02 15:02:42 +1000

    + Added greybox base for wolf spawning

---
+++ Assets/Scripts/CreatureSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureSpawner : MonoBehaviour {
+
+	public GameObject SpawnedCreature;
+	public HexChunk AttachedHexChunk;
+	public GameObject PlayerInScene;
+
+	public float SpawnDelay;
+	public float SpawnJitter;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
